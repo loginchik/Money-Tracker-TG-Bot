bb2d644d25a4d3eb2f77846106939ddd181ff1bc
Anna Loginova <zv1376a@gmail.com>
2024-04-24 18:20:38 +0300

    Delete router messages are multilingual now

--- bot/routers/delete_router.py
+++ bot/routers/delete_router.py
@@ -3,9 +3,10 @@ Package contains scripts to address delete queries to db. Supports user data del
 Runs on its own router - ``delete_router`` which must be included into main router or any other router
 that is included into main to be able to get and handle pending updates.
 """
-
+import logging
 
 from aiogram import Router
+from aiogram.enums import ParseMode
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery
@@ -16,6 +17,7 @@ from bot.states.registration import DataDeletionStates
 from bot.keyboards.bool_keyboard import generate_bool_keyboard
 from bot.filters.user_exists import UserExists
 from bot.static.user_languages import USER_LANGUAGE_PREFERENCES
+from bot.static.messages import DELETE_ROUTER_MESSAGES
 
 
 delete_router = Router()
@@ -23,30 +25,34 @@ delete_router.message.middleware(UserLanguageMiddleware())
 
 
 @delete_router.message(Command(commands=['delete_my_data']), ~UserExists())
-async def nothing_to_delete_message(message: Message):
-    await message.answer('You are not registered, so there is no data to delete.')
+async def nothing_to_delete_message(message: Message, user_lang: str):
+    message_text = DELETE_ROUTER_MESSAGES['nothing_to_delete'][user_lang]
+    await message.answer(message_text)
 
 
 @delete_router.message(Command(commands=['delete_my_data']), UserExists())
 async def delete_user_data(message: Message, state: FSMContext, user_lang: str):
     decision_keyboard = await generate_bool_keyboard(user_lang)
     await state.set_state(DataDeletionStates.decision)
-    await message.answer('Are you sure? The action cannot be undone.', reply_markup=decision_keyboard)
+    message_text = DELETE_ROUTER_MESSAGES['confirmation'][user_lang]
+    await message.answer(message_text, reply_markup=decision_keyboard, parse_mode=ParseMode.HTML)
 
 
 @delete_router.callback_query(DataDeletionStates.decision)
-async def save_delete_choice(callback: CallbackQuery, state: FSMContext):
+async def save_delete_choice(callback: CallbackQuery, state: FSMContext, user_lang: str):
     await callback.message.edit_reply_markup(reply_markup=None)
     if callback.data == 'true':
         user_id = callback.from_user.id
         try:
             await db.user_operations.delete_user_data(user_id)
             del USER_LANGUAGE_PREFERENCES[user_id]
-            await callback.message.answer('All data deleted.')
+            message_text = DELETE_ROUTER_MESSAGES['success'][user_lang]
+            await callback.message.answer(message_text)
         except Exception as e:
-            print(e)
-            await callback.message.answer('Sorry, try again later')
+            logging.error(e)
+            message_text = DELETE_ROUTER_MESSAGES['error'][user_lang]
+            await callback.message.answer(message_text)
     else:
-        await callback.message.answer('We are glad you are not deleting')
-
+        message_text = DELETE_ROUTER_MESSAGES['cancel'][user_lang]
+        await callback.message.answer(message_text)
     await state.clear()
---
+++ bot/static/messages.py
@@ -0,0 +1,26 @@
+DELETE_ROUTER_MESSAGES = {
+    'nothing_to_delete': {
+        'ru': 'Вы не зарегистрированы, так что нет данных для удаления',
+        'en': 'You are not registered, so there is no data to delete.'
+    },
+    'confirmation': {
+        'ru': 'Вы уверены? Данные удаляются тут же и безвозвратно, '
+              '<b>действие не может быть отменено</b>',
+        'en': 'Are you sure? Data will be deleted immediately and forever. '
+              '<b>The action cannot be undone</b>'
+    },
+    'success': {
+        'ru': 'Все данные, связанные с вами, удалены. Всего хорошего!',
+        'en': 'All data associated with you is deleted. Thank you!'
+    },
+    'error': {
+        'ru': 'К сожалению, произошла внутренняя ошибка, данные сохранены. '
+              'Пожалуйста, попробуйте позже',
+        'en': 'Unfortunately, internal error occurred, data is still saved. '
+              'Please try again.'
+    },
+    'cancel': {
+        'ru': 'Данные сохранены. Спасибо, что остаётесь с нами!',
+        'en': 'Your data is kept in safe. Thank you for staying with us!'
+    }
+}
