29e1d039e6c478357f7fa3c1e3bc02436e198cd8
Anna Loginova <zv1376a@gmail.com>
2024-04-24 15:04:56 +0300

    Periods update

--- bot/keyboards/limit_period_keyboard.py
+++ bot/keyboards/limit_period_keyboard.py
@@ -2,9 +2,9 @@ from aiogram.utils.keyboard import InlineKeyboardMarkup, InlineKeyboardButton, I
 
 
 async def generate_period_keyboard(user_language_code: str) -> InlineKeyboardMarkup:
-    week = InlineKeyboardButton(text='Неделя' if user_language_code == 'ru' else 'Week', callback_data='period:1')
-    month = InlineKeyboardButton(text='Месяц' if user_language_code == 'ru' else 'Month', callback_data='period:2')
-    year = InlineKeyboardButton(text='Год' if user_language_code == 'ru' else 'Year', callback_data='period:3')
+    week = InlineKeyboardButton(text='7 дней' if user_language_code == 'ru' else '7 days', callback_data='period:1')
+    month = InlineKeyboardButton(text='30 дней' if user_language_code == 'ru' else '30 days', callback_data='period:2')
+    year = InlineKeyboardButton(text='365 дней' if user_language_code == 'ru' else '365 days', callback_data='period:3')
 
     keyboard = InlineKeyboardBuilder()
     keyboard.add(week)
