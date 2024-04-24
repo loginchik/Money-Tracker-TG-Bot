5a68a70f0aa66901dc0293d8d5d1bd3dd03c0193
Anna Loginova <zv1376a@gmail.com>
2024-04-24 18:06:29 +0300

    Help text

--- bot/routers/general_router.py
+++ bot/routers/general_router.py
@@ -1,4 +1,5 @@
 from aiogram import Router
+from aiogram.enums import ParseMode
 from aiogram.types import Message
 from aiogram.filters import Command, StateFilter, CommandStart
 
@@ -17,7 +18,17 @@ async def start_message(message: Message, user_lang: str):
 
 @general_router.message(Command('help'), StateFilter(None))
 async def help_message(message: Message, user_lang: str):
-    await message.answer('Help!')
+    descr_column = 'ru_long' if user_lang == 'ru' else 'en_long'
+    command_descriptions = [
+        f'/{command_text}\n{command_descr[descr_column]}' for command_text, command_descr in commands.items()
+        if command_text not in ['abort', 'help']
+    ]
+    commands_text = '\n\n'.join([f'({i + 1}) {text}' for i, text in enumerate(command_descriptions)])
+
+    help_heading = 'Доступные команды' if user_lang == 'ru' else 'Commands available'
+    help_heading = '<b>' + help_heading + '</b>'
+    message_text = '\n\n'.join([help_heading, commands_text])
+    await message.answer(message_text, parse_mode=ParseMode.HTML)
 
 
 @general_router.message(Command('about'), StateFilter(None))
--- bot/static/commands.py
+++ bot/static/commands.py
@@ -14,14 +14,23 @@ commands = {
     'add_income': {
         'en': 'Add new income',
         'ru': 'Добавить новый доход',
-        'en_long': '',
-        'ru_long': '',
+        'en_long': "Log new income: money amount, active or passive mark, date. /abort command stops the process "
+                   "and erases temp data (previously logged incomes won't change).",
+        'ru_long': "Добавить новый доход: сумма, активный или пассивный, дата. Команда /abort прервёт процесс и "
+                   "удалит все временные данные (сохранённые ранее доходы не пострадают).",
     },
     'add_expense_limit': {
         'en': 'Add new expense limit for subcategory',
         'ru': 'Добавить предел расходов по подкатегории',
-        'en_long': '',
-        'ru_long': '',
+        'en_long': "Add new expense limit. Expense limits don't affect expense logging but help to keep track "
+                   "of expenses by subcategories. Expense limits are set for a specified period of time "
+                   "(7/30/365 days). Expired limits are deleted automatically. /abort command stops the process "
+                   "and erases temp data (previously created expense limits won't change).",
+        'ru_long': "Добавить новый предел расходов, связанный с подкатегорией расходов. Пределы расходов не "
+                   "ограничивают возможность вносить расходы, но помогают контролировать их. Пределы расходов "
+                   "устанавливаются на определённый временной период (7/30/365 дней). Просроченные пределы удаляются"
+                   "автоматически. Команда /abort прервёт процесс и удалит все временные данные (созданные ранее "
+                   "пределы расходов не пострадают).",
     },
     'help': {
         'en': 'Get help on bot functions and commands',
@@ -32,8 +41,8 @@ commands = {
     'about': {
         'en': 'About the project',
         'ru': 'О проекте',
-        'en_long': '',
-        'ru_long': '',
+        'en_long': 'Project goals and history.',
+        'ru_long': 'История и цели проекта.',
     },
     'abort': {
         'en': 'Abort current process and erase temp data',
@@ -44,8 +53,8 @@ commands = {
     'delete_my_data': {
         'en': 'Delete all one-related data from database',
         'ru': 'Удалить все связанные с пользователем данные из базы данных',
-        'en_long': '',
-        'ru_long': '',
+        'en_long': 'Delete all the user data. The action cannot be undone.',
+        'ru_long': 'Удалить все данные пользователя. Действие необратимо.',
     }
 }
 
