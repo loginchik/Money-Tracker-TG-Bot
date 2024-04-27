8a5e1ff50d41f996f04ae719533af74f873c2f5b
Anna Loginova <zv1376a@gmail.com>
2024-04-27 03:13:08 +0300

    Db scripts update on update balance

--- db/db_setup/functions/update_balance_on_new_expense.sql
+++ db/db_setup/functions/update_balance_on_new_expense.sql
@@ -1,11 +1,11 @@
-CREATE OR REPLACE FUNCTION update_balance_on_new_expense()
+CREATE OR REPLACE FUNCTION user_based.update_balance_on_new_expense()
 RETURNS TRIGGER AS
 $$
 BEGIN
 	UPDATE user_based.expense_limit SET current_balance = current_balance - NEW."amount"
 	WHERE user_id = NEW.user_id
 	AND subcategory = NEW.subcategory
-	AND current_period_start <= now() AND current_period_end >= now();
+	AND current_period_start <= now();
 	RETURN NULL;
 END
 $$
